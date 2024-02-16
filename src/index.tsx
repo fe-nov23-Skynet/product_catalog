96c8dc3cf9f0150685597b09db5886c4d18f414a
Yaroslav Samoilenko <yasik2255@live.com>
2024-02-16 14:09:19 +0200

    added icons/fonts/styles, installed scss, created theme colors

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
