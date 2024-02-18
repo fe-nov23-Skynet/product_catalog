acf52e5b9cba0272d837f098c36fbf167c36b22b
Yaroslav Samoilenko <yasik2255@live.com>
2024-02-18 20:38:21 +0200

    drop-down v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Select } from './components/Select/Select';
 
 function App() {
+  const a = ['Phone 1', 'Size 2', 'Tablet 3', 'Screen 4', 'Size 2'];
+
   return (
     <div className="App">
       test vercel deploy with redeploy by commit
-      <Select />
+      <Select items={a} />
     </div>
   );
 }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable arrow-parens */
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+import { v4 as uuidv4 } from 'uuid';
 
 import Styles from './Select.module.scss';
 
-export const Select:React.FC = () => {
+interface Props {
+  items: string[];
+  selectedItem?: string;
+  onSelect?: (item: string) => void;
+}
+
+export const Select:React.FC<Props> = (props) => {
+  const { items, onSelect = () => {}, selectedItem } = props;
+
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState(selectedItem || '');
+  const rootRef = useRef<HTMLDivElement>(null);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const a = ['Phone 1', 'Size 2', 'Tablet 3', 'Screen 4', 'Size 2'];
   const placeHolder = 'Select value';
 
   function handleList() {
@@ -19,6 +28,7 @@ export const Select:React.FC = () => {
   function selectItem(value: string) {
     setSelectedValue(value);
     setIsOpen(false);
+    onSelect(value);
   }
 
   function handleEnter(
@@ -30,6 +40,25 @@ export const Select:React.FC = () => {
     }
   }
 
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      const { target } = event;
+      if (target instanceof Node && !rootRef.current?.contains(target)) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('click', handleClick);
+
+    return () => {
+      window.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  /* function handleBlur(e: React.FocusEvent<HTMLDivElement, Element>):void {
+    e.target
+  } */
+
   const openStyle = Styles['list_wrapper--open'];
   const closeIcon = Styles['select_icon--close'];
 
@@ -39,10 +68,14 @@ export const Select:React.FC = () => {
     <div
       className={Styles.select}
       onClick={handleList}
+      ref={rootRef}
+      /* onBlur={handleBlur} */
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
       tabIndex={0}
     >
-      {selectedValue || placeHolder}
+      <p className={Styles.select_title}>
+        {selectedValue || placeHolder}
+      </p>
       <div className={classNames(`${Styles.select_icon}`, {
         [closeIcon]: isOpen,
       })}
@@ -53,7 +86,7 @@ export const Select:React.FC = () => {
       })}
       >
         <ul className={Styles.select_list}>
-          {a.map(
+          {items.map(
             item => (
               // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
               <li
@@ -62,6 +95,7 @@ export const Select:React.FC = () => {
                 tabIndex={0}
                 onClick={() => selectItem(item)}
                 onKeyUp={e => handleEnter(e, item)}
+                key={uuidv4()}
               >
                 {item}
               </li>
