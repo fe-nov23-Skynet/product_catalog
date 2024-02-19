cd7a1ed22439bef3699decf9b1943d29277fc7a8
Yaroslav Samoilenko <yasik2255@live.com>
2024-02-19 17:23:38 +0200

    added mixins, Select fixes

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,13 +2,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { App } from './App';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+import { Catalog } from './pages/Catalog';
 
 export const Root = () => (
   <Routes>
     <Route path="/" element={<App />}>
       <Route index element={<h1 className="title">Home Page</h1>} />
       <Route path="home" element={<Navigate to="/" />} />
-      <Route path="phones" element={<h1 className="title">Phones Page</h1>} />
+      <Route path="phones" element={<Catalog />} />
       <Route path="tablets" element={<h1 className="title">Tablets Page</h1>} />
       <Route path="accessories" element={<h1 className="title">Accessories Page</h1>} />
       <Route path="favourites" element={<h1 className="title">Favourites Page</h1>} />
