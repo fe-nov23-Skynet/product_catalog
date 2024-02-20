979ce5644cb383b9560a82bbfa2131cdeb594691
VasiliyYarosh <vasilliy.yarosh@gmail.com>
2024-02-20 21:22:59 +0200

    Added basic breadbrumbs logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Breadcrumbs } from './components/Breadcrumbs/Breadcrumbs';
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
 
       <div className="section">
         <div className="container">
+          <Breadcrumbs />
           <Outlet />
         </div>
       </div>
