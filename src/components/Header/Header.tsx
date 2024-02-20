3026c886dc674f4a933610ce835f203d0f8b2384
ValeriaMyronets <valery.mir.11@gmail.com>
2024-02-20 22:02:48 +0200

    8693ur3q5_burgerMenu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,73 +3,167 @@
 import './header.scss';
 import { Link, NavLink } from 'react-router-dom';
 import cn from 'classnames';
+import { useState } from 'react';
 import { ReactComponent as Favorites } from '../../styles/icons/Favourites.svg';
 import { ReactComponent as Cart } from '../../styles/icons/Cart.svg';
+import { ReactComponent as Menu } from '../../styles/icons/menu.svg';
+import { ReactComponent as Close } from '../../styles/icons/close.svg';
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const navItemClassName = (
     { isActive }: { isActive: boolean },
-  ) => cn('header_nav__link', {
+  ) => cn({
+    header_nav__link: !menuOpen,
+    nav__link: menuOpen,
     'header_nav__link--active': isActive,
   });
 
+  const changeMenuOpen = () => setMenuOpen(!menuOpen);
+
   return (
-    <header className="header">
-      <NavLink
-        to="/"
-        className="header_logo__img"
-      />
+    <>
+      <header className="header">
+        <NavLink
+          to="/"
+          className="header_logo__img"
+          onClick={() => setMenuOpen(false)}
+        />
 
-      <div className="header_container">
-        <nav className="header_nav">
-          <NavLink
-            to="/"
-            className={navItemClassName}
-          >
-            HOME
-          </NavLink>
-
-          <NavLink
-            to="phones"
-            className={navItemClassName}
-          >
-            PHONES
-          </NavLink>
-
-          <NavLink
-            to="tablets"
-            className={navItemClassName}
-          >
-            TABLETS
-          </NavLink>
-
-          <NavLink
-            to="accessories"
-            className={navItemClassName}
-          >
-            ACCESSORIES
-          </NavLink>
-        </nav>
-        <div className="header_box">
-          <div className="header_box__container">
-            <Link
-              to="favourites"
-              className="header_box__link"
+        <div className="header_container">
+          <nav className="header_nav">
+            <NavLink
+              to="/"
+              className={navItemClassName}
             >
-              <Favorites />
-            </Link>
-          </div>
+              HOME
+            </NavLink>
 
-          <div className="header_box__container">
-            <Link
-              to="cart"
-              className="header_box__link"
+            <NavLink
+              to="phones"
+              className={navItemClassName}
             >
-              <Cart />
-            </Link>
+              PHONES
+            </NavLink>
+
+            <NavLink
+              to="tablets"
+              className={navItemClassName}
+            >
+              TABLETS
+            </NavLink>
+
+            <NavLink
+              to="accessories"
+              className={navItemClassName}
+            >
+              ACCESSORIES
+            </NavLink>
+          </nav>
+
+          <div className="header_box">
+            <div className="header_box__container">
+              <Link
+                to="favourites"
+                className="header_box__link"
+              >
+                <Favorites />
+              </Link>
+            </div>
+
+            <div className="header_box__container">
+              <Link
+                to="cart"
+                className="header_box__link"
+              >
+                <Cart />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+
+        {!menuOpen ? (
+          <button className="burger-toggle" onClick={changeMenuOpen}>
+            <Menu />
+          </button>
+        ) : (
+          <button className="burger-toggle" onClick={changeMenuOpen}>
+            <Close />
+          </button>
+        )}
+      </header>
+
+      {menuOpen && (
+        <div className="menu">
+          <nav className="nav nav-menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink
+                  className={navItemClassName}
+                  to="/"
+                  onClick={changeMenuOpen}
+                >
+                  HOME
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  className={navItemClassName}
+                  to="phones"
+                  onClick={changeMenuOpen}
+                >
+                  PHONES
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  className={navItemClassName}
+                  to="tablets"
+                  onClick={changeMenuOpen}
+                >
+                  TABLETS
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  className={navItemClassName}
+                  to="accessories"
+                  onClick={changeMenuOpen}
+                >
+                  ACCESSORIES
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="menu__bottom">
+            <NavLink
+              to="favourites"
+              className={(
+                { isActive }: { isActive: boolean },
+              ) => cn('menu_item', {
+                'header_nav__link--active': isActive,
+              })}
+              onClick={changeMenuOpen}
+            >
+              <Favorites />
+            </NavLink>
+
+            <NavLink
+              to="cart"
+              className={(
+                { isActive }: { isActive: boolean },
+              ) => cn('menu_item', {
+                'header_nav__link--active': isActive,
+              })}
+              onClick={changeMenuOpen}
+            >
+              <Cart />
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
