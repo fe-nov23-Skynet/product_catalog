4db3335f923212fa3c22164c88c1eb83d162c19a
Yaroslav Samoilenko <yasik2255@live.com>
2024-02-21 18:03:15 +0200

    realized main info about product with responsive

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -23,17 +23,18 @@ export const ProductPage: React.FC/* <Props> */ = (/* props */) => {
 
       <h2 className="product-page__title">{product.name}</h2>
 
-      <div className="product-page__specs">
-        <span className="product-page__id">{`ID: ${numericID}`}</span>
-
+      <div className="product-page__info">
         <div className="product-page__images">
           <img src={product.images[0]} alt={product.name} />
         </div>
 
         <div className="product-page__settings">
-          <div className="product-page__colors">
+          <div className="colors__header">
             <span className="product-page__colors-title">Available colors</span>
+            <span className="product-page__id id--on-mobile">{`ID: ${numericID}`}</span>
+          </div>
 
+          <div className="product-page__colors">
             <ul className="product-page__list">
               <li><input type="radio" /></li>
               <li><input type="radio" /></li>
@@ -67,27 +68,6 @@ export const ProductPage: React.FC/* <Props> */ = (/* props */) => {
             </span>
           </div>
 
-          <h3 className="card__product-info">
-            <p className="card__product-info-prop">
-              Screen
-            </p>
-            <p className="card__product-info-value">
-              {product.screen}
-            </p>
-            <p className="card__product-info-prop">
-              Capacity
-            </p>
-            <p className="card__product-info-value">
-              {product.capacity}
-            </p>
-            <p className="card__product-info-prop">
-              RAM
-            </p>
-            <p className="card__product-info-value">
-              {product.ram}
-            </p>
-          </h3>
-
           <div className="card__submit-container">
             <a
               className="card__button-submit"
@@ -105,6 +85,97 @@ export const ProductPage: React.FC/* <Props> */ = (/* props */) => {
             </button>
           </div>
 
+          <h3 className="card__product-info">
+            <p className="card__product-info-prop">
+              Screen
+            </p>
+            <p className="card__product-info-value">
+              {product.screen}
+            </p>
+            <p className="card__product-info-prop">
+              Resolution
+            </p>
+            <p className="card__product-info-value">
+              {product.resolution}
+            </p>
+            <p className="card__product-info-prop">
+              Processor
+            </p>
+            <p className="card__product-info-value">
+              {product.processor}
+            </p>
+            <p className="card__product-info-prop">
+              RAM
+            </p>
+            <p className="card__product-info-value">
+              {product.ram}
+            </p>
+          </h3>
+        </div>
+        <span className="product-page__id text-s-12 id--on-desktop">{`ID: ${numericID}`}</span>
+        <div className="product-page__about">
+          <h3>About</h3>
+          <hr />
+          {product.description.map((description) => (
+            <>
+              <h4>{description.title}</h4>
+              <p>{description.text}</p>
+            </>
+          ))}
+        </div>
+        <div className="product-page__specs">
+          <h3>Tech specs</h3>
+          <hr />
+          <h3 className="card__product-info">
+            <p className="card__product-info-prop">
+              Screen
+            </p>
+            <p className="card__product-info-value">
+              {product.screen}
+            </p>
+            <p className="card__product-info-prop">
+              Resolution
+            </p>
+            <p className="card__product-info-value">
+              {product.resolution}
+            </p>
+            <p className="card__product-info-prop">
+              Processor
+            </p>
+            <p className="card__product-info-value">
+              {product.processor}
+            </p>
+            <p className="card__product-info-prop">
+              RAM
+            </p>
+            <p className="card__product-info-value">
+              {product.ram}
+            </p>
+            <p className="card__product-info-prop">
+              Built in memory
+            </p>
+            <p className="card__product-info-value">
+              {product.capacity}
+            </p>
+            <p className="card__product-info-prop">
+              Camera
+            </p>
+            <p className="card__product-info-value">
+              {product.camera}
+            </p>
+            <p className="card__product-info-prop">
+              Zoom
+            </p>
+            <p className="card__product-info-value">
+              {product.zoom}
+            </p>
+            <p className="card__product-info-prop">
+              Cell
+            </p>
+            <p className="card__product-info-value">
+              {product.cell}
+            </p>
+          </h3>
         </div>
       </div>
     </section>
