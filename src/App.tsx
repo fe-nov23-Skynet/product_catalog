16577415979dce8dc46ff913ccf178ab2dace261
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-02-23 20:35:28 +0200

    swiper task 8693ur3kp done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Breadcrumbs } from './components/Breadcrumbs/Breadcrumbs';
+import { SliderBar } from './components/Swiper';
 
 export function App() {
   return (
     <div className="App">
       <Header />
+
+      <SliderBar />
 
       <main className="section">
         <div className="container">
