0073e8917c39505092c96fefbec3b92286113cca
ValeriaMyronets <valery.mir.11@gmail.com>
2024-02-23 21:13:03 +0200

    Changing logic cart

--- src/features/cartSlice.ts
+++ src/features/cartSlice.ts
@@ -7,6 +7,7 @@ export type Category = string;
 
 interface CartItem extends Product {
   count: number;
+  category: string;
 }
 
 interface CartActionItem {
@@ -38,6 +39,7 @@ export const cartSlice = createSlice({
         const newItem = {
           ...item,
           count: count + 1,
+          category,
         };
         state.cartProducts = [...state.cartProducts, newItem];
         state.itemsCount += 1;
