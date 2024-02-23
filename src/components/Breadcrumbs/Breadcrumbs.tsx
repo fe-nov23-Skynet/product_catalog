4e0381a96a0eac9026145232770e531991246715
VasiliyYarosh <vasilliy.yarosh@gmail.com>
2024-02-23 16:53:48 +0200

    Final breadcrumbs solution

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import './breadcrumbs.scss';
 import cn from 'classnames';
+import { Fragment } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { ReactComponent as Home } from '../../styles/icons/Home_breadcrumbs.svg';
 import { ReactComponent as Arrow } from '../../styles/icons/Chevron_arrow_breadcrumbs.svg';
@@ -12,21 +13,29 @@ export function Breadcrumbs() {
   const crumbs = location.pathname.split('/')
     .filter(crumb => crumb !== '')
     .map((crumb, index, array) => {
+      const formattedCrumb = crumb
+        .split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+
       currentLink += `/${crumb}`;
 
       const isLast = index === array.length - 1;
       const linkClasses = cn('crumb_link', { 'crumb_link--active': isLast });
 
       return (
-        <>
-          {index === 0 && <Home />}
-          <div className="crumb" key={crumb}>
+        <Fragment key={formattedCrumb}>
+          {index === 0 && (
+            <Link to="/">
+              <Home />
+            </Link>
+          )}
+          <div className="crumb" key={formattedCrumb}>
             <p className="crumb_arrow">
               <Arrow />
             </p>
-            <Link className={linkClasses} to={currentLink}>{crumb}</Link>
+            <Link className={linkClasses} to={currentLink}>{formattedCrumb}</Link>
           </div>
-        </>
+        </Fragment>
       );
     });
 
