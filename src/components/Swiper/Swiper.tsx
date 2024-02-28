ca762ff79464ecafc8afc49a64fae6c6299023ab
Yaroslav Samoilenko <129086393+Xilston@users.noreply.github.com>
2024-02-28 22:27:06 +0200

    Update Swiper.tsx
    remove fade

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -22,7 +22,6 @@ export const SliderBar = () => (
         modules={[Pagination, Navigation, Autoplay, EffectFade]}
         spaceBetween={20}
         slidesPerView={1}
-        effect="fade"
         autoplay={{
           delay: 2500,
           disableOnInteraction: true,
