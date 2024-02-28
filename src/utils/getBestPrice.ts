c72142959b853f10b6377bb18c71a7b8fbd19669
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-02-28 23:34:47 +0200

    nightmare fixes

--- src/utils/getBestPrice.ts
+++ src/utils/getBestPrice.ts
@@ -1,7 +1,10 @@
 import { Product } from '../types/Product';
 
 export function getBestPrice(products: Product[]) {
-  const bestPrice = products.filter(prod => prod.priceRegular - prod.priceDiscount > 0);
+  const bestPrice = products
+    .filter(prod => prod.priceRegular - prod.priceDiscount > 0)
+    .filter(prod => prod.name.length > 28)
+    .filter(prod => prod.screen.length < 25);
   bestPrice.sort((a, b) => (b.priceRegular - b.priceDiscount) - (a.priceRegular - a.priceDiscount));
 
   return bestPrice;
