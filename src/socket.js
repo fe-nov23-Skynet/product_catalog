e4b81272bc1f13551acc68dd9c6a7dd0a84f403c
Yaroslav Samoilenko <yasik2255@live.com>
2024-03-12 15:13:02 +0200

    added server integration with chating

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:4000'; // 'production' ? undefined : 'http://localhost:4000';
+const URL = 'https://fe-nov-23-products-catalog-backend.onrender.com/'; // 'production' ? undefined : 'http://localhost:4000';
 
 export const socket = io(URL, {
   autoConnect: true,
